1e21c0b60045ab664d3ed309ec0dacd455d16118
Aaqil Jaffer <xm9363cy@go.minnstate.edu>
2026-08-18 12:39:19 -0500

    Create UserRepository interface for user retrieval

---
+++ src/main/java/edu/metro/branding/UserRepository.java
@@ -0,0 +1,9 @@
+package edu.metro.branding.repository;
+
+import edu.metro.branding.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
